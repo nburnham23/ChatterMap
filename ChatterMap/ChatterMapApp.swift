2b171d8ed690e00ab4d92b1381376a7d7e30fc69
jared3333 <jaredfcollins@gmail.com>
2025-10-08 12:17:32 -0400

    Add files via upload

---
+++ ChatterMap/ChatterMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatterMapApp.swift
+//  ChatterMap
+//
+//  Created by jared on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatterMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
