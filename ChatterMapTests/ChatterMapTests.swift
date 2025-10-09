8f0991a83de298b5ca4a096d1942506487aed10e
jared <jaredfcollins@gmail.com>
2025-10-09 13:59:31 -0400

    copied files to here

---
+++ ChatterMapTests/ChatterMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatterMapTests.swift
+//  ChatterMapTests
+//
+//  Created by jared on 10/8/25.
+//
+
+import Testing
+@testable import ChatterMap
+
+struct ChatterMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
