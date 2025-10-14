e9d255d0558c5fdebf57d18ba971bbdaf492f959
Noah <noahburnham23@gmail.com>
2025-10-14 13:14:02 -0400

    Added BTV map location

---
+++ ChatterMap/ChatterMapView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChatterMap
+//
+//  Created by jared on 10/8/25.
+//
+
+// a comment
+
+import SwiftUI
+import MapKit
+
+struct ChatterMapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 44.4759, longitude: -73.2121), // Burlington, VT
+        span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+    )
+    var body : some View {
+        Map(coordinateRegion: $region)
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    ContentView()
+}
