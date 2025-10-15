6b482e2816f194dd98fd461e6135ca4c0162b8f5
jared <jaredfcollins@gmail.com>
2025-10-15 12:53:33 -0400

    added new views and connected them in home view

--- ChatterMap/ChatterMapView.swift
+++ ChatterMap/ChatterMapView.swift
@@ -12,10 +12,27 @@ import MapKit
 
 
 struct ChatterMapView: View {
+    @State private var showRoutesView = false
+    @State private var showNewNoteView = false
+    @State private var showProfileView = false
+    
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 44.4759, longitude: -73.2121), // Burlington, VT
         span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
     )
+    
+    func toggleRoutesView() {
+        showRoutesView = !showRoutesView
+    }
+    
+    func toggleNewNoteView() {
+        showNewNoteView = !showNewNoteView
+    }
+    
+    func toggleProfileView() {
+        showProfileView = !showProfileView
+    }
+    
     var body : some View {
         Map(coordinateRegion: $region)
             .ignoresSafeArea()
@@ -25,6 +42,9 @@ struct ChatterMapView: View {
         Spacer()
         Spacer()
         HStack{
+            if !showRoutesView && !showNewNoteView && !showProfileView {
+              Text("Back")
+            }
             Spacer()
             Button("routes", action: {})
             Spacer()
