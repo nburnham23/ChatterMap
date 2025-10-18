f61e87839d722573793a1464418d298594dc7d3b
Noah <noahburnham23@gmail.com>
2025-10-18 12:20:57 -0400

    allowed location, added locaiton manager class

--- ChatterMap/ChatterMapView.swift
+++ ChatterMap/ChatterMapView.swift
@@ -12,10 +12,13 @@ import MapKit
 
 
 struct ChatterMapView: View {
+    @StateObject private var locationManager = LocationManager()
     @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 44.4759, longitude: -73.2121), // Burlington, VT
+        center: CLLocationCoordinate2D(latitude: locationManager.lastKnownLocation?.latitude, longitude: locationManager.lastKnownLocation?.longitude), // Burlington, VT
         span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
     )
+    
+    
     var body : some View {
         Map(coordinateRegion: $region)
             .ignoresSafeArea()
@@ -27,10 +30,22 @@ struct ChatterMapView: View {
         HStack{
             Spacer()
             Button("routes", action: {})
+                .frame(width: 100, height: 100)
+                .foregroundColor(Color.white)
+                .background(Color.blue)
+                .clipShape(Circle())
             Spacer()
             Button("write note", action: {})
+                .frame(width: 100, height: 100)
+                .foregroundColor(Color.white)
+                .background(Color.blue)
+                .clipShape(Circle())
             Spacer()
             Button("profile", action: {})
+                .frame(width: 100, height: 100)
+                .foregroundColor(Color.white)
+                .background(Color.blue)
+                .clipShape(Circle())
             Spacer()
         }
         Spacer()
