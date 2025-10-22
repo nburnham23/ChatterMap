2c3c5656dc038bb6921d2ce995e92538e10a46dc
Noah <noahburnham23@gmail.com>
2025-10-22 12:14:56 -0400

    Got user location to work and added view if user location not permitted

---
+++ ChatterMap/LocationDeniedView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDeniedView.swift
+//  ChatterMap
+//
+//  Created by Noah Burnham on 10/22/25.
+//
+
+import SwiftUI
+
+struct LocationDeniedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDeniedView()
+}
